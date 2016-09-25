a53b6eee3c35a4959cdc57b437a8b896adbd1fb4
Aldira Putra R <dira.thea@gmail.com>
2016-09-25 20:19:35 +0700

    remove throw and add log for error

--- FacebookHandler.js
+++ FacebookHandler.js
@@ -19,10 +19,14 @@ bot.on('message', (payload, reply) => {
     let text = payload.message.text;
 
     bot.getProfile(payload.sender.id, (err, profile) => {
-        if (err) throw err;
+        if (err) {
+            console.log('GET_PROFILE_ERROR', err);
+        }
 
         reply({text}, (err) => {
-            if (err) throw err;
+            if (err) {
+                console.log('REPLY_ERROR', err);
+            }
 
             console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
         })
