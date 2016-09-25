2ca285897a2d5c1c3c591b902268832a271f42c5
Aldira Putra R <dira.thea@gmail.com>
2016-09-25 22:00:06 +0700

    Game Timeout Action

--- GameHandler.js
+++ GameHandler.js
@@ -32,7 +32,7 @@ class GameHandler {
                     };
                     this.timeout[playerId] = {
                         time: Date.now(),
-                        id: setTimeout(this._timeoutAction(timeoutAction), TIMEOUT_DURATION),
+                        id: setTimeout(this._timeoutAction.bind(this, playerId, timeoutAction), TIMEOUT_DURATION),
                         action: timeoutAction,
                     };
                     resolve({
@@ -63,7 +63,7 @@ class GameHandler {
                 const timeRemaining = TIMEOUT_DURATION - (Date.now() - this.timeout[playerId].time);
                 const quiz = this.listener[playerId];
                 this.listener[playerId].index += 1;
-                this.timeout[playerId].id = setTimeout(this._timeoutAction(this.timeout[playerId].action), timeRemaining);
+                this.timeout[playerId].id = setTimeout(this._timeoutAction.bind(this, playerId, this.timeout[playerId].action), timeRemaining);
                 resolve({
                     index: quiz.index + 1,
                     total: quiz.clues.length,
