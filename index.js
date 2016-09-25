1d96b2be3dbfe10ad3b875f5bec40c05bc21170e
Aldira Putra R <dira.thea@gmail.com>
2016-09-25 19:26:56 +0700

    Test the webhook

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const webApp = express();
+webApp.use(bodyParser.json()); // for parsing application/json
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -29,12 +31,19 @@ bot.on('message', function allMessage(message) {
 });
 
 webApp.get('/webhook', (req, res) => {
+    console.log(req.body);
+    // Verify webhook
     if (req.query['hub.verify_token'] === 'URBAN_QUIZ_VERIFY') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Error, wrong validation token');
     }
+
+    //  Something else
+    console.log(req.body);
 });
+
+//EAACirKiZCWvwBAE1IpBwmCgLGMH5m8f3NJ2GpkzTD0q1xB4egb5zuii5N9CQDfnINF4Igwe4rtkOESq0VGRVhIaZBIFCsKuQuTSk3kneDZC6cJkHZARwNlAhlTtsT5QG4Vs7lVdnvFY7CSAwXHDntGQ6MXfAmXuis3wn4S5sSAZDZD
 
 // Heroku Handler for webapps
 webApp.listen(process.env.PORT, function () {
