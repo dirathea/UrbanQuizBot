15fd5c836a6c27e1f2958b6fe2bf021a7800364c
Aldira Putra R <dira.thea@gmail.com>
2016-10-01 18:15:40 +0700

    Setup webhook for Telegram

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const WEBHOOK_URL = process.env.WEBHOOK_URL || 'localhost';
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -13,9 +14,7 @@ webApp.use(bodyParser.urlencoded({
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Setup polling way
-const bot = new TelegramBot(token, {
-    polling: true
-});
+const bot = new TelegramBot(token);
 
 const Handler = require('./handler.js');
 const FacebookHandler = require('./FacebookHandler');
@@ -47,8 +46,15 @@ webApp.post('/webhook', (req, res) => {
     res.end(JSON.stringify({status: 'ok'}));
 });
 
+webApp.post(`/${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
 // Heroku Handler for webapps
 webApp.listen(process.env.PORT, function () {
     console.log('service is started');
     facebookHandler.initializeThreadSettings();
 });
+
+bot.setWebHook(`${WEBHOOK_URL}/${token}`);
