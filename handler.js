5e9a2b0b9585cffc9a52969bc79a8ade74c3872e
Aldira Putra R <dira.thea@gmail.com>
2016-10-02 23:34:25 +0700

    GameHandler for Telegram

--- handler.js
+++ handler.js
@@ -2,21 +2,21 @@
 const Promise = require("bluebird");
 const Scrapper = require('./scrapper');
 const ChatBottleHandler = require('./ChatBottleHandler');
+const GameHandler = require('./GameHandler');
 const Botan = require('botanio');
 
 const TIMEOUT_IN_SECOND = 30;
 const TIMEOUT_DURATION = TIMEOUT_IN_SECOND * 1000;
-const quizStatement = "Guess the word by the following definition: \n\n";
+const quizStatement = "Guess the word by the following definition:";
 const rateUrbanQuiz = "\nRate UrbanQuizBot on : https://telegram.me/storebot?start=urbanquizbot";
 
 const botan = Botan(process.env.BOTAN_TOKEN);
 const chatBottleHandler = new ChatBottleHandler();
+const gameHandler = new GameHandler();
 
 class Handler {
     constructor(bot) {
         this.bot = bot;
-        this.listener = [];
-        this.timeout = [];
     }
 
     _sendMessage(chatId, content, options) {
@@ -26,78 +26,44 @@ class Handler {
     startGameProcessor(message) {
         chatBottleHandler.incomingMessageProcessor(message);
         botan.track(message, 'start');
-        if (!this.listener[message.chat.id]) {
-            let scrapper = new Scrapper();
-            return Promise.resolve().then(() => {
-                if (this.listener[message.chat.id]) {
-                    return Promise.reject('Game Already Running');
-                }
-                return scrapper.getWord();
-            }).then((result) => {
-                const firstClue = `${quizStatement}(1/${result.length})\n${result[0].meaning}`;
-                Promise.join(this._sendMessage(message.chat.id, firstClue),
-                    Promise.resolve(result),
-                    (sent, quiz) => {
-                        chatBottleHandler.outgoingMessageProcessor(sent.message_id, firstClue, message.chat.id);
-                        this.listener[message.chat.id] = {
-                            index: 1,
-                            clues: quiz,
-                        };
-                        this.timeout[message.chat.id] = {
-                            time: Date.now(),
-                            id: setTimeout(this._quizTimeout.bind(this, message.chat.id), TIMEOUT_DURATION),
-                        };
-                    }
-                )
-                ;
-            }).catch((error) => {
-                console.log(error);
-            });
-        }
+        return gameHandler.startGame(message.chat.id, (answer) => {
+            this._quizTimeout(message.chat.id, answer);
+        }).then((result) => {
+            const firstClue = `${quizStatement}\n${result.hidden}\n(${result.index}/${result.total})\n${result.clue}`;
+            this._sendMessage(message.chat.id, firstClue);
+        }).catch((error) => {
+            console.log(error);
+        });
     }
 
     answerProcessor(message) {
-        if (this.listener[message.chat.id] && !message.text.startsWith('/')) {
-            if (this.listener[message.chat.id].clues[0].word.toLowerCase() == message.text.toLowerCase().trim()) {
-                clearTimeout(this.timeout[message.chat.id].id);
-
-                const correctAnswer = `Great! The right answer is : ${this.listener[message.chat.id].clues[0].word}${rateUrbanQuiz}`;
-
+        if (!message.text.startsWith('/')) {
+            gameHandler.answerQuiz(message.chat.id, message.text().toLowerCase()).then((answer) => {
+                const correctAnswer = `Great! The right answer is : ${answer}${rateUrbanQuiz}`;
                 this._sendMessage(message.chat.id, correctAnswer).then((msg) => {
                     chatBottleHandler.outgoingMessageProcessor(msg.message_id, correctAnswer, message.chat.id);
                 });
-                this.listener[message.chat.id] = undefined;
-                this.timeout[message.chat.id] = undefined;
-            }
+            }).catch((err) => {
+
+            });
         }
     }
 
     getClueProcessor(message) {
         chatBottleHandler.incomingMessageProcessor(message);
         botan.track(message, 'newclue');
-        if (this.listener[message.chat.id] && this.listener[message.chat.id].index < this.listener[message.chat.id].clues.length) {
-            clearTimeout(this.timeout[message.chat.id].id);
-
-            const timeRemaining = TIMEOUT_DURATION - (Date.now() - this.timeout[message.chat.id].time);
-            const quiz = this.listener[message.chat.id];
-            const newClue = `${quizStatement}(${quiz.index + 1}/${quiz.clues.length})\n${quiz.clues[quiz.index].meaning}`;
-
-            this._sendMessage(message.chat.id, newClue).then((msg) => {
-                chatBottleHandler.outgoingMessageProcessor(msg.message_id, newClue, message.chat.id);
-                this.listener[message.chat.id].index += 1;
-                this.timeout[message.chat.id].id = setTimeout(this._quizTimeout.bind(this, message.chat.id), Math.max(timeRemaining, 0));
-            });
-        }
+        gameHandler.requestNewClue(message.chat.id).then((quiz) => {
+            const newClue = `${quizStatement}\n${quiz.hidden}\n(${quiz.index}/${quiz.total})\n${quiz.clue}`;
+            this._sendMessage(message.chat.id, newClue);
+        });
     }
 
-    _quizTimeout(chatId) {
-        const timeoutMessage = `Timeout! The right answer for previous question is : ${this.listener[chatId].clues[0].word}${rateUrbanQuiz}`;
+    _quizTimeout(chatId, answer) {
+        const timeoutMessage = `Timeout! The right answer for previous question is : ${answer}${rateUrbanQuiz}`;
 
         this._sendMessage(chatId, timeoutMessage).then((msg) => {
             chatBottleHandler.outgoingMessageProcessor(msg.message_id, timeoutMessage, chatId);
         });
-        this.listener[chatId] = undefined;
-        this.timeout[chatId] = undefined;
     }
 }
 
