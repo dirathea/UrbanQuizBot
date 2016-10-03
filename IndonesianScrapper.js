3541f20a05630f4ce9591b8d41327f1a41f8b679
Aldira Putra R <dira.thea@gmail.com>
2016-10-03 23:21:13 +0700

    Scrapper random ina

--- IndonesianScrapper.js
+++ IndonesianScrapper.js
@@ -8,6 +8,7 @@ const retry = require('bluebird-retry');
 const osmosis = require('osmosis');
 const _ = require('lodash');
 
+const KITAB_GAUL_BASIC_ENDPOINT = 'https://kitabgaul.com';
 const KITAB_GAUL_RANDOM_ENDPOINT = 'https://kitabgaul.com/words/random';
 
 class IndonesianScrapper {
@@ -23,36 +24,54 @@ class IndonesianScrapper {
         });
     }
 
+    _getRandomWord() {
+        return new Promise((resolve, reject) => {
+            let word = '';
+            osmosis.get(KITAB_GAUL_RANDOM_ENDPOINT)
+                .find('.entryDetail .word a')
+                .set({
+                    'slug': '@href'
+                })
+                .data((result) => {
+                    word = result.slug;
+                })
+                .done(() => {
+                    resolve(word);
+                });
+        });
+    }
+
     _getWord() {
         return new Promise(function (resolve, reject) {
             let quiz = [];
-            osmosis.get(KITAB_GAUL_RANDOM_ENDPOINT)
-                .click('.word a')
-                .find('.entryDetail')
-                .set({
-                    'word': '.word a',
-                    'meaning': '.definition'
-                })
-                .data(function (result) {
-                    if (!_.isEmpty(result) && result.meaning.length < 200) {
-                        quiz.push(result);
-                    }
-                })
-                .done(function () {
-                    console.log('total of clue', quiz.length);
+            this._getRandomWord().then((slug) => {
+                osmosis.get(`${KITAB_GAUL_BASIC_ENDPOINT}/${slug}`)
+                    .find('.entryDetail')
+                    .set({
+                        'word': '.word a',
+                        'meaning': '.definition'
+                    })
+                    .data(function (result) {
+                        if (!_.isEmpty(result) && result.meaning.length < 200) {
+                            quiz.push(result);
+                        }
+                    })
+                    .done(function () {
+                        console.log('total of clue', quiz.length);
 
-                    if (quiz.length > 0) {
-                        resolve(quiz.slice(0, 3).map((clue) => {
-                            clue.meaning = clue.meaning.replace(new RegExp(clue.word, 'g'), '_____');
-                            return clue;
-                        }));
-                    } else {
-                        reject('Failed to get clue');
-                    }
-                })
-                .log(console.log)
-                .error(console.log)
-                .debug(console.log);
+                        if (quiz.length > 0) {
+                            resolve(quiz.slice(0, 3).map((clue) => {
+                                clue.meaning = clue.meaning.replace(new RegExp(clue.word, 'g'), '_____');
+                                return clue;
+                            }));
+                        } else {
+                            reject('Failed to get clue');
+                        }
+                    })
+                    .log(console.log)
+                    .error(console.log)
+                    .debug(console.log);
+            });
         });
     }
 }
